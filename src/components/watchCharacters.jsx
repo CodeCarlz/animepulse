e3a0d7b4853db18941cbcbb89cf521f7848d8417
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-02-10 15:44:50 +0800

    fix image responsiveness

--- src/components/watchCharacters.jsx
+++ src/components/watchCharacters.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Button from "./button";
 import { animeInfo } from "@/utils/animeInfo";
 import Card from "./card";
+import Image from "next/image";
 console.log(animeInfo.characters);
 
 const Watchcharacters = () => {
@@ -27,38 +28,48 @@ const Watchcharacters = () => {
         </Button>
       </div>
       <div
-        className=" h-[1000px] w-screen md:w-[95vw] ml-0 mt-5 gap-2 grid grid-cols-4 grid-rows-7 
+        className=" h-[1000px] w-screen md:w-[95vw] ml-0 mt-5 gap-2  grid grid-cols-4 grid-rows-7 
       sm:grid-cols-5 sm:grid-rows-5 sm:h-[700px]
-      md:grid-cols-4 md:grid-rows-7 md:h-[1300px]
-      lg:grid-cols-5 lg:grid-rows-6 lg:h-[1200px]
+      md:grid-cols-4 md:grid-rows-7 md:h-[1300px] md:gap-5
+      lg:grid-cols-5 lg:grid-rows-6 lg:h-[1300px]
       xl:grid-cols-6 xl:grid-rows-5 xl:h-[1100px]
       2xl:grid-cols-7 2xl:grid-rows-4 2xl:h-[1000px]
-    
+
       "
       >
-        {animeInfo.characters.map((element, index) => (
-          <div
-            className={`h-[100px] w-[100px] hover:h-[110px] hover:w-[110px] text-white hover:text-pink-600
-            md:w-[150px] md:h-[150px] md:hover:w-[160px]  md:hover:h-[160px]
+        {/* {
+          h-[100px] w-[100px] hover:h-[110px] hover:w-[110px]
+          md:w-[150px] md:h-[150px] md:hover:w-[160px]  md:hover:h-[160px]
             lg:w-[180px] lg:h-[180px] lg:hover:w-[190px]  lg:hover:h-[190px]
             2xl:w-[200px] 2xl:h-[200px] 2xl:hover:w-[210px]  2xl:hover:h-[210px]
+        } */}
+        {animeInfo.characters.map((element, index) => (
+          <div
+            className={` text-white hover:text-pink-600
+            max-h-[100px] 
+            md:max-h-[150px]
+            lg:max-h-[180px]
+            2xl:max-h-[200px]
+            transition-transform ease-in-out duration-300 transform hover:scale-110
             `}
             key={index}
           >
             <div
-              className={`relative h-full bg-zinc-900 rounded-lg overflow-hidden text-white`}
+              className={`relative h-full bg-zinc-900 rounded-lg overflow-hidden text-white `}
             >
-              <img
+              <Image
                 src={element.image}
+                width="100"
+                height="100"
                 alt=""
                 className="h-full w-full hover:opacity-60"
               />
               <p
-                className={`absolute top-0 right-0 text-sm  px-2 ${
-                  element.role.toLocaleLowerCase() === "main"
-                    ? "bg-pink-600"
-                    : element.role.toLocaleLowerCase() === "supporting"
-                    ? "bg-green-600"
+                className={`absolute top-0 right-0 text-[12px] rounded-bl-lg ${
+                  element.role.toLowerCase() === "main"
+                    ? "bg-pink-600 px-2"
+                    : element.role.toLowerCase() === "supporting"
+                    ? "bg-green-600 px-1"
                     : ""
                 }`}
               >
