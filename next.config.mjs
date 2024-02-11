048cefc7be9691bacd30f101fea1079d40ea4cec
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-02-11 18:37:20 +0800

    fix api domain host

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "s4.anilist.co",
       },
+      {
+        protocol: "https",
+        hostname: "media.kitsu.io",
+      },
+      {
+        protocol: "https",
+        hostname: "artworks.thetvdb.com",
+      },
     ],
   },
 };
