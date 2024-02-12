383e5acc94f12d7815d19223d5973131dfa9978d
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-02-13 06:55:36 +0800

    make a reusable endpoint for API fetching

---
+++ src/services/animeData.js
@@ -0,0 +1,32 @@
+// import { axiosInstance } from "@/services/api";
+// import {
+//   INFO_ANIME,
+//   POPULAR_ANIME,
+//   RECENT_ANIME,
+//   SEARCH_ANIME,
+//   TRENDING_ANIME,
+// } from "./endpoint";
+
+// export const randomAnime = async () => {
+//   const response = await axiosInstance.get(RECENT_ANIME);
+// };
+
+// export const popularAnime = async () => {
+//   const response = await axiosInstance.get(POPULAR_ANIME);
+// };
+
+// export const trendingAnime = async () => {
+//   const response = await axiosInstance.get(TRENDING_ANIME);
+// };
+
+// export const recentAnime = async () => {
+//   const response = await axiosInstance.get(recentAnime);
+// };
+
+// export const searchAnime = async () => {
+//   const response = await axiosInstance.get(SEARCH_ANIME);
+// };
+
+// export const infoAnime = async () => {
+//   const response = await axiosInstance.get(INFO_ANIME);
+// };
---
+++ src/services/endpoint.js
@@ -0,0 +1,6 @@
+export const RANDOM_ANIME = "/meta/anilist/random-anime";
+export const POPULAR_ANIME = "/meta/anilist/popular";
+export const TRENDING_ANIME = "/meta/anilist/trending";
+export const RECENT_ANIME = "/meta/anilist/recent-episodes";
+export const SEARCH_ANIME = "/meta/anilist/";
+export const INFO_ANIME = "/meta/anilist/info/";
