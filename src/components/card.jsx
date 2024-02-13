5dbbc94d4af0aa56c1266ae843812f59d9af0ad2
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-02-13 22:55:05 +0800

    add errorHandling for anime title

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,19 +4,27 @@ import Image from "next/image";
 const Card = ({ element, index }) => {
   return (
     <Link
-      href={`/watch/${element?.title.english
-        .replace(/\s/g, "-")
+      href={`/watch/${(element?.title.english
+        ? element?.title.english
+        : element?.title.romaji
+        ? element?.title.romaji
+        : element?.title.native
+      )
         .replace(/.*: /, "")
-        .toLocaleLowerCase()}/${element?.id}`}
+        .replace(/\s/g, "-")
+        .toLowerCase()}/${element?.id}`}
       className="flex h-[300px] w-[300px] min-w-[267px] rounded-xl overflow-hidden ml-2 mr-3"
       key={index}
     >
       <div className="flex flex-col justify-center items-center gap-2 py-5 bg-gradient-to-r from-gray-700">
         <p className="flex items-end white flex-1 h-screen w-[50px] writing-mode-vertical-rl whitespace-nowrap pb-5 max-h-[90vh] overflow-hidden">
           <span className="-rotate-90 max-w-full text-md ">
-            {element?.title.english.length > 25
-              ? `${element?.title.english.slice(0, 25)}...`
-              : element.title.english}
+            {element?.title.english
+              ? element?.title.english
+              : element?.title.romaji
+              ? element?.title.romaji
+              : element?.title.native}
+            {console.log(element.title)}
           </span>
         </p>
         <p className="text-2xl font-semibold text-green-300 ">{index + 1}</p>
