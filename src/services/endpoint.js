f2e53352dcd3e1c31fc854babb5d5d506ed58473
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-02-15 20:59:10 +0800

    fix type for endpoints url

--- src/services/endpoint.js
+++ src/services/endpoint.js
@@ -3,5 +3,5 @@ export const POPULAR_ANIME = "/meta/anilist/popular";
 export const TRENDING_ANIME = "/meta/anilist/trending";
 export const RECENT_ANIME = "/meta/anilist/recent-episodes";
 export const SEARCH_ANIME = "/meta/anilist/";
-export const INFO_ANIME = "/meta/anilist/info/";
-export const WATCH_ANIME = "/meta/anilist/watch/";
+export const INFO_ANIME = "/meta/anilist/info";
+export const WATCH_ANIME = "/meta/anilist/watch";
