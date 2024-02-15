1857a1807196697b3c3752356d6f0c7fa5401d95
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-02-15 23:35:11 +0800

    fix mobile responsiveness

--- src/components/cover.jsx
+++ src/components/cover.jsx
@@ -8,20 +8,20 @@ const Cover = ({ element }) => {
   return (
     <>
       <div
-        className="relative h-[600px] pb-10  justify-between flex items-end bg-cover bg-center bg-no-repeat "
+        className="relative h-[400px] md:h-[500px] lg:h-[600px] pb-10  justify-between flex items-end bg-cover bg-center bg-no-repeat "
         style={{ backgroundImage: `url("${element?.cover}")` }}
       >
         <div className="absolute inset-0 bg-gradient-to-tr from-gray-900 to-transparent z-3 "></div>
         <div className="absolute inset-0 bg-gradient-to-tl from-gray-900 to-transparent z-3 "></div>
         <div
-          className="z-10  w-2/4 flex flex-col justify-end gap-5 pl-3 md:pl-5 lg:pl-10
+          className="z-10  w-[60vw] md:w-2/4 flex flex-col justify-end gap-2 md:gap-3 lg:gap-5 pl-3 md:pl-5 lg:pl-10
           transition-transform duration-1000 ease-in-out
       "
         >
-          <h1 className="text-green-400 text-xl lg:text-3xl">
+          <h1 className="text-green-400 text-sm md:text-lg lg:text-3xl">
             #{element?.rating} <span className="text-pink-600">Spotlight</span>
           </h1>
-          <h1 className="text-white text-2xl lg:text-4xl font-semibold tracking-wide">
+          <h1 className="text-white text-xl md:text-2xl lg:text-4xl font-semibold tracking-wide">
             {element.title.english ??
               element.title.romaji ??
               element.title.native}
@@ -30,20 +30,24 @@ const Cover = ({ element }) => {
             <label htmlFor="" className={"flex items-center gap-1"}>
               <label
                 htmlFor=""
-                className="h-4 w-4 p-[4px] bg-white  rounded-full flex justify-center items-center"
+                className="h-3 w-3 p-[3px]  bg-white  rounded-full flex justify-center items-center"
               >
                 <FaPlay className="" />
               </label>
-              <p className="text-white">{element.type}</p>
+              <p className="text-white text-sm md:text-md lg:text-lg">
+                {element.type}
+              </p>
             </label>
             <label htmlFor="" className={"flex items-center gap-1"}>
               <label
                 htmlFor=""
-                className="h-4 w-4 p-[4px] bg-white  rounded-full flex justify-center items-center"
+                className="h-3 w-3 p-[1px] bg-white  rounded-full flex justify-center items-center"
               >
                 <CiCalendarDate />
               </label>
-              <p className="text-white">{element.releaseDate}</p>
+              <p className="text-white text-sm md:text-md lg:text-lg">
+                {element.releaseDate}
+              </p>
             </label>
 
             <label htmlFor="">
