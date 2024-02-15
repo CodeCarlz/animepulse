0b4b10b137d4b05d38a866978feadec15351d972
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-02-15 14:35:25 +0800

    install hls dependencies and add playm3u8 function

--- src/components/artplayer.jsx
+++ src/components/artplayer.jsx
@@ -2,16 +2,41 @@
 import { useEffect, useRef } from "react";
 import Artplayer from "artplayer";
 import { MdOutlineSubtitles } from "react-icons/md";
+import Hls from "hls.js";
 
 export default function Player({ animeVideo, option, getInstance, ...rest }) {
   const artRef = useRef();
+
+  function playM3u8(video, url, art) {
+    if (Hls.isSupported()) {
+      if (art.hls) art.hls.destroy();
+      const hls = new Hls();
+      hls.loadSource(url);
+      hls.attachMedia(video);
+      art.hls = hls;
+      art.on("destroy", () => hls.destroy());
+    } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+      video.src = url;
+    } else {
+      art.notice.show = "Unsupported playback format: m3u8";
+    }
+  }
+
+  let select = [];
+  animeVideo.sources.map((e) => {
+    if (e.quality === "backup" || e.quality === "default") return;
+    select.unshift({
+      html: e.quality,
+      url: `https://m3u8-proxy-cors-lime.vercel.app/cors?url=${e.url}`,
+    });
+  });
 
   useEffect(() => {
     const art = new Artplayer({
       ...option,
       container: artRef.current,
-      url: `/Aurora.mp4`,
-      poster: "/one_piece.jpg",
+      url: `https://www081.vipanicdn.net/streamhls/0789fd4f049c3ca2a49b860ea5d1f456/ep.1.1703882952.360.m3u8`,
+      // poster: "/one_piece.jpg",
       volume: 0.5,
       isLive: false,
       muted: false,
@@ -36,6 +61,9 @@ export default function Player({ animeVideo, option, getInstance, ...rest }) {
       lang: navigator.language.toLowerCase(),
       moreVideoAttr: {
         crossOrigin: "anonymous",
+      },
+      customType: {
+        m3u8: playM3u8,
       },
       settings: [
         {
