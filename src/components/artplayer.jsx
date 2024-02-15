ef81be1662f0af39692595442dc4bf8809d92b39
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-02-15 10:45:56 +0800

    add artplayer component

---
+++ src/components/artplayer.jsx
@@ -0,0 +1,80 @@
+"use client";
+import { useEffect, useRef } from "react";
+import Artplayer from "artplayer";
+import { MdOutlineSubtitles } from "react-icons/md";
+
+export default function Player({ animeVideo, option, getInstance, ...rest }) {
+  const artRef = useRef();
+
+  useEffect(() => {
+    const art = new Artplayer({
+      ...option,
+      container: artRef.current,
+      url: `/Aurora.mp4`,
+      poster: "/one_piece.jpg",
+      volume: 0.5,
+      isLive: false,
+      muted: false,
+      autoplay: false,
+      pip: true,
+      autoSize: false,
+      screenshot: true,
+      setting: true,
+      loop: true,
+      flip: false,
+      playbackRate: false,
+      aspectRatio: true,
+      fullscreen: true,
+      fullscreenWeb: true,
+      miniProgressBar: true,
+      mutex: true,
+      backdrop: true,
+      playsInline: true,
+      autoPlayback: true,
+      airplay: true,
+      theme: "#23ade5",
+      lang: navigator.language.toLowerCase(),
+      moreVideoAttr: {
+        crossOrigin: "anonymous",
+      },
+      settings: [
+        {
+          width: 100,
+          html: "Quality",
+          tooltip: "Auto",
+          icon: `<img width="22" heigth="22" src=""`,
+          selector: [
+            {
+              html: "360P",
+              tooltip: "Show",
+              switch: true,
+              onSwitch: function (item) {
+                item.tooltip = item.switch ? "Hide" : "Show";
+                art.subtitle.show = !item.switch;
+                return !item.switch;
+              },
+            },
+          ],
+          onSelect: function (item) {
+            art.subtitle.switch(item.url, {
+              name: item.html,
+            });
+            return item.html;
+          },
+        },
+      ],
+    });
+
+    if (getInstance && typeof getInstance === "function") {
+      getInstance(art);
+    }
+
+    return () => {
+      if (art && art.destroy) {
+        art.destroy(false);
+      }
+    };
+  }, []);
+
+  return <div ref={artRef} {...rest} className="w-full   h-full"></div>;
+}
