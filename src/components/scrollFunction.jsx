276c937a39501f7c3c8eb39693da0098a7ee58d8
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-02-16 10:26:05 +0800

    fix the card container for mobile responsiveness

--- src/components/scrollFunction.jsx
+++ src/components/scrollFunction.jsx
@@ -27,7 +27,7 @@ const Scrollfunction = ({ anime, title }) => {
   };
 
   return (
-    <div className="relative  max-w-[full]  text-white h-[25rem] flex justify-start items-center gap-2 md:gap-4 lg:gap-6 transition delay-5000 overflow-hidden">
+    <div className="relative  max-w-[full]  text-white h-[21rem] md:h-[23rem] lg:h-[24rem] flex justify-start items-center gap-2 md:gap-4 lg:gap-6 transition delay-5000 overflow-hidden">
       <p className="absolute top-1 left-7 text-pink-600 text-3xl">{title}</p>
       {!anime ? (
         ""
