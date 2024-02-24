26f741be8367047c194d19248ce111929abe5582
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-02-24 16:14:19 +0800

    make a function for input container

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,24 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Slider from "@/components/slider";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaSearch } from "react-icons/fa";
 import Button from "@/components/button";
 import useScrolled from "@/utils/useScrolled";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Header = ({ isScrolled, scrolledInput }) => {
   const scrolled = useScrolled();
+
+  const [inputValue, SetInputValue] = useState(null);
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const url = `/filter/${inputValue}`;
+    router.push(url);
+  };
 
   return (
     <header
@@ -23,7 +33,8 @@ const Header = ({ isScrolled, scrolledInput }) => {
         <Link href={"/home"}>
           <img src="/logo.png" alt="" className="h-10 w-48" />
         </Link>
-        <div
+        <form
+          onSubmit={handleSubmit}
           className={` p-1 rounded-xl hidden lg:flex  ${
             scrolled
               ? "bg-gradient-to-t from-neutral-300"
@@ -35,11 +46,13 @@ const Header = ({ isScrolled, scrolledInput }) => {
           <input
             type="text"
             className={`pl-2 outline-none bg-transparent text-black font-semibold `}
+            value={inputValue}
+            onChange={(e) => SetInputValue(e.target.value)}
           />
-          <button className="p-2">
+          <button className="p-2" type="submit">
             <FaSearch />
           </button>
-        </div>
+        </form>
       </div>
 
       <div className="flex justify-center items-center">
