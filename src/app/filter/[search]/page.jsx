4a0e4fca9eaa86ed5b9e250f4ef117ffa3793b7a
CodeCarlz <johnkarl.limjoco@gmail.com>
2024-02-29 08:38:32 +0800

    fix the responsiveness of main container and also call the listCard component

--- src/app/filter/[search]/page.jsx
+++ src/app/filter/[search]/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import ListCard from "@/components/listCard";
 import SearchCard from "@/components/searchCard";
 import { axiosInstance } from "@/services/api";
 import { SEARCH_ANIME } from "@/services/endpoint";
@@ -31,12 +32,21 @@ const Page = ({ params }) => {
         isScrolled={"bg-gradient-to-t from-pink-900"}
         scrolledInput={"bg-gradient-to-t from-neutral-300"}
       />
-      <main className="pt-[50px] lg:pt-[70px] px-5 h-[1200px]">
-        <div className=" h-full grid grid-cols-5 grid-rows-3 gap-5">
-          {searchAnime?.results?.map((element, index) => (
-            <SearchCard element={element} key={index} />
-          ))}
-          {/* {console.log(searchAnime)} */}
+      <main className=" pt-[50px] lg:pt-[70px] px-5 h-full min-h-[900px] py-10">
+        <div className="grid grid-cols-[1fr] lg:grid-cols-[1fr_350px]  gap-5">
+          {/* <h1 className="text-pink-600 text-3xl">Filter</h1> */}
+          <div className=" h-full grid grid-cols-3 md:grid-cols-4 xl:grid-cols-5 auto-rows-max gap-5">
+            {searchAnime?.results?.map((element, index) => (
+              <SearchCard element={element} key={index} />
+            ))}
+          </div>
+
+          <div>
+            <ListCard
+              cardTitle={"Top Rated"}
+              anime={searchAnime?.results.slice(0, 10)}
+            />
+          </div>
         </div>
       </main>
       <Footer />
